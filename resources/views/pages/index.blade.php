8df21bb0fe2f2bab3efafb2de6c519f6d832dd21
Arno Klein <binarybottle@gmail.com>
2020-04-03 13:49:25 -0400

    Update index.blade.php

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -46,8 +46,9 @@
         in the context of everyone else’s, and to create a collection of
         publicly shared recordings or transcripts.</p>
 
-        <p>*Our partners include <a href="https://openhumans.org">Open Humans</a>,
-        and
+        <p>*Our list of partners is growing, and presently includes 
+        <a href="https://openhumans.org">Open Humans</a>,
+        the <a href="https://cri-paris.org>CRI</a> in Paris, and
         <a href="https://mcgovern.mit.edu/profile/satrajit-ghosh/">Satrajit Ghosh</a>
         and Sanu Abraham at MIT.
 
