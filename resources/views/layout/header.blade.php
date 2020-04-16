298031aa72a00b0b751cf94a4d131c0798bb0af7
henryrossiter <henry.rossiter@utexas.edu>
2020-04-16 16:04:03 +0000

    Switch logos

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -6,10 +6,10 @@
     <div class="container">
         <div class="row">
             <div class="col-6 col-md-3 my-auto">
-                <a href="/"><img src="{{ asset('media/logos/CrisisLogger_logo.png') }}" alt="{{ config('app.name') }}"></a>
+                <img src="{{ asset('media/logos/parents_magazine_logo.png') }}" alt="{{ config('app.name') }}" id="parents_magazine_logo">
             </div>
             <div class="col-6 col-md-3 my-auto">
-                <img src="{{ asset('media/logos/parents_magazine_logo.png') }}" alt="{{ config('app.name') }}" id="parents_magazine_logo">
+                <a href="/"><img src="{{ asset('media/logos/CrisisLogger_logo.png') }}" alt="{{ config('app.name') }}"></a>
             </div>
             <div class="col-12 col-md-6 my-auto mx-auto">
 
