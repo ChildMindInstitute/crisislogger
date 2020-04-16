3722cf8f6f69ba025ef069d79729fb75a5364784
Anton-Matyunin <anton.matyunin1101@outlook.com>
2020-04-16 06:25:59 -0700

    fix 500 error issue on account page

--- resources/views/pages/capture/create-account.blade.php
+++ resources/views/pages/capture/create-account.blade.php
@@ -20,7 +20,7 @@
 
                     <div class="form-group">
                         <label>Email address<span class="text-danger">*</span></label>
-                        <input type="email" class="form-control @error('email') is-invalid @enderror" aria-describedby="emailHelp" name="email" required placeholder="Enter email">
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" value="{{old('email')}}" aria-describedby="emailHelp" name="email" required placeholder="Enter email">
                         <span class="form-text text-muted">We'll never share your email with anyone else.</span>
                         @error('email')
                         <span class="invalid-feedback" role="alert">
@@ -31,7 +31,7 @@
 
                     <div class="form-group">
                         <label>Name</label>
-                        <input type="text" class="form-control" aria-describedby="nameHelp" name="name" placeholder="Enter full name">
+                        <input type="text" class="form-control"  value="{{old('password')}}"  aria-describedby="nameHelp" name="name" placeholder="Enter full name">
                         <span class="form-text text-muted">Your full name.</span>
                     </div>
 
