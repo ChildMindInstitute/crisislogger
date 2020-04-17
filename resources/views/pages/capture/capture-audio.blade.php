d64401bb7327af3588f2d9d101cf31e3c0a2c5a6
Sam <sam@Sams-Mac.local>
2020-04-17 05:55:32 -0700

    fix the issue 47

--- resources/views/pages/capture/capture-audio.blade.php
+++ resources/views/pages/capture/capture-audio.blade.php
@@ -11,16 +11,11 @@
 
                 <div>
                     <div class="recorder_wrapper">
-                        <div class="recorder">
-                            <button class="record_btn" id="button">
-                                <svg class="bi bi-mic-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M5 3a3 3 0 016 0v5a3 3 0 01-6 0V3z"/>
-                                    <path fill-rule="evenodd" d="M3.5 6.5A.5.5 0 014 7v1a4 4 0 008 0V7a.5.5 0 011 0v1a5 5 0 01-4.5 4.975V15h3a.5.5 0 010 1h-7a.5.5 0 010-1h3v-2.025A5 5 0 013 8V7a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
-                                </svg>
-                            </button>
+                        <div class="recorder record_btn" id="recorder" style="margin: auto; text-align: center;justify-content: center; align-items: center;display: flex">
+
                         </div>
                     </div>
-                    
+
                     <!-- Modal -->
                     <div class="modal fade" id="reviewRecordingModal" tabindex="-1" role="dialog" aria-labelledby="reviewRecordingModal" aria-hidden="true">
                         <div class="modal-dialog" role="document">
@@ -50,13 +45,13 @@
                 and what is helping you get through this crisis.</b>
                 Your recording can be up to 5 minutes long.
                 You will be able to save the recording for private use
-                or share it publicly, but                
+                or share it publicly, but
                 please avoid using any identifying names or information.
                 It will be transcribed by Google's transcription service,
                 and you will be able to view a word cloud created from the transcript.
                 We hope that you will come back and record more as a journal of your thoughts and experiences.</p>
 
-                <p>If you would prefer, you can  
+                <p>If you would prefer, you can
                 <a href="{{ route('capture-video') }}">record video</a> or
                 <a href="{{ route('capture-text') }}">type text</a> instead.</p>
 
