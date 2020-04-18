72561a39846f93d92a59861914a4d7ed8d426464
Asaad Riaz <riaz.asaad@gmail.com>
2020-04-18 16:34:52 -0700

    fix checkboxes

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -13,53 +13,57 @@
             <div class="card col-lg-12">
                 <div class="card-body">
                     <div class="row">
-                    @if(count($uploads) > 0)
+                        @if(count($uploads) > 0)
 
                             @foreach($uploads  as $upload)
-                            <div class="col-sm-12 col-md-4 col-lg-3 mb-4 " style="box-shadow: 2px 1px 4px 4px #e0dbdb;">
-                                <div class="kt-portlet" >
-                                    <div class="kt-portlet__head">
-                                        <div class="kt-portlet__head-label">
-                                            <h3 class="kt-portlet__head-title">
-                                                {{ $upload->created_at }}
-                                            </h3>
+                                <div class="col-sm-12 col-md-4 col-lg-3 mb-4 " style="box-shadow: 2px 1px 4px 4px #e0dbdb;">
+                                    <div class="kt-portlet" >
+                                        <div class="kt-portlet__head">
+                                            <div class="kt-portlet__head-label">
+                                                <h3 class="kt-portlet__head-title">
+                                                    {{ $upload->created_at }}
+                                                </h3>
+                                            </div>
+                                        </div>
+                                        <div class="kt-portlet__body" >
+                                            @if(Str::contains($upload->link, '.wav'))
+                                                <audio controls src="{{ $upload->link }}" style="width: 100%"></audio>
+                                            @else
+                                                <video controls>
+                                                    <source src="{{ $upload->link }}" type="video/webm">
+                                                    Your browser does not support the video tag.
+                                                </video>
+                                            @endif
                                         </div>
                                     </div>
-                                    <div class="kt-portlet__body" >
-                                        @if(Str::contains($upload->link, '.wav'))
-                                            <audio controls src="{{ $upload->link }}" style="width: 100%"></audio>
-                                        @else
-                                            <video controls>
-                                                <source src="{{ $upload->link }}" type="video/webm">
-                                                Your browser does not support the video tag.
-                                            </video>
-                                        @endif
+                                    @if(isset($upload->transcript->id))
+                                        <div class="kt-portlet"  id="{{'transcript-'.$upload->id}}">
+                                            @include('components.spinner')
+                                        </div>
+                                    @else
+                                        <div class="kt-portlet " id="{{'transcript-'.$upload->id}}">
+{{--                                            <p class="card-title text-center">No word cloud</p>--}}
+                                        </div>
+                                    @endif
+                                    <div class="kt-portlet__body--fit-bottom   remove-btn">
+                                        <div class="form-group float-left mb-0">
+                                            <input type="checkbox" class="form-check-input contribute-to-science"  value="{{$upload->contribute_to_science}}" {{$upload->contribute_to_science?'checked': ''}}  id="contribute-upload-{{$upload->id}}">
+                                            <label class="form-check-label text-black" for="contribute-upload-{{$upload->id}}">Science?</label>
+                                        </div>
+                                        <div class="form-group float-left mb-0">
+                                            <input type="checkbox" class="form-check-input contribute-to-science"  value="{{$upload->share}}"   {{$upload->share?'checked': ''}}  id="share-upload-{{$upload->id}}">
+                                            <label class="form-check-label text-black" for="share-upload-{{$upload->id}}">Public?</label>
+                                        </div>
+                                        <a class="remove-resource" href="#" id="upload-{{$upload->id}}" ><i class="fa fa-trash" style="margin-right: 10px" ></i> Delete?</a>
                                     </div>
                                 </div>
-                                @if(isset($upload->transcript->id))
-                                    <div class="kt-portlet"  id="{{'transcript-'.$upload->id}}">
-                                        @include('components.spinner')
-                                    </div>
-                                @else
-                                    <div class="kt-portlet " id="{{'transcript-'.$upload->id}}">
-{{--                                        <p class="card-title text-center">No word cloud</p>--}}
-                                    </div>
-                                @endif
-                                <div class="kt-portlet__body--fit-bottom   remove-btn">
-                                    <div class="form-group form-check float-left mb-0">
-                                        <input type="checkbox" class="form-check-input contribute-to-science"  value="{{$upload->contribute_to_science}}" {{$upload->contribute_to_science?'checked': ''}}  id="contribute-upload-{{$upload->id}}">
-                                        <label class="form-check-label" for="contribute">Contribute to science</label>
-                                    </div>
-                                    <a class="float-right" href="{{route('remove', ['id' => $upload->id, 'type' => 'upload'])}}" ><i class="fa fa-trash" ></i> Remove</a>
-                                </div>
-                            </div>
                             @endforeach
                         @else
 
-                        <div class="text-center mt-5">
-                            <h4>No uploads found.</h4>
-                        </div>
-                    @endif
+                            <div class="text-center mt-5">
+                                <h4>No uploads found.</h4>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
@@ -71,29 +75,42 @@
             <div class="card col-lg-12">
                 <div class="card-body">
                     @if(count($texts) > 0)
-                    <div class="row">
+                        <div class="row">
 
                             @foreach($texts  as $text)
-                                <div class="col-sm-12 col-md-4 col-lg-3">
-                                    <div class="kt-portlet">
+                                <div class="col-sm-12 col-md-4 col-lg-3 mb-4 " style="box-shadow: 2px 1px 4px 4px #e0dbdb;">
+                                    <div class="kt-portlet text-content" id="text-content-{{$text->id}}">
                                         <p class="text-justify text-ellipsis">{{$text->text}}</p>
                                     </div>
-                                    <div class="kt-portlet__body--fit-bottom">
-                                        <div class="form-group form-check float-left mb-0">
+                                    <div class="kt-portlet__body--fit-bottom" style="display: inline-grid">
+                                        <div class="form-group float-left mb-0">
                                             <input type="checkbox" class="form-check-input contribute-to-science"  value="{{$text->contribute_to_science}}"   {{$text->contribute_to_science?'checked': ''}}  id="contribute-text-{{$text->id}}">
-                                            <label class="form-check-label" for="contribute">Contribute to science</label>
+                                            <label class="form-check-label text-black" for="contribute-text-{{$text->id}}">Science?</label>
                                         </div>
-                                        <a class="float-right" href="{{route('remove', ['id' => $text->id, 'type' => 'text'])}}"><i class="fa fa-trash" ></i> Remove</a>
+                                        <div class="form-group float-left mb-0">
+                                            <input type="checkbox" class="form-check-input contribute-to-science"  value="{{$text->share}}"   {{$text->share?'checked': ''}}  id="share-text-{{$text->id}}">
+                                            <label class="form-check-label text-black" for="share-text-{{$text->id}}">Public?</label>
+                                        </div>
+                                        <a class="remove-resource" href="#" id="text-{{$text->id}}"><i class="fa fa-trash" style="margin-right: 10px" ></i> Delete?</a>
                                     </div>
                                 </div>
                             @endforeach
 
-                    </div>
+                        </div>
                     @else
-                    <div class="text-center mt-5">
+                        <div class="text-center mt-5">
                             <h4>No uploads found.</h4>
                         </div>
                     @endif
+                </div>
+            </div>
+            <div class="modal fade" id="text-content-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -104,8 +121,42 @@
         height: 100px;
         width: 100%;
         left: 0;
-        bottom: -50px;
+        bottom: -10px;
         padding: 15px;
+        display: inline-grid;
+        position: absolute;
+    }
+    a {
+        font-size: 13px;
+        color: #000000 !important;
+    }
+    input[type="checkbox"] {
+        visibility: hidden;
+    }
+    label {
+        cursor: pointer;
+    }
+    input[type="checkbox"] + label:before {
+        border: 1px solid #0067a0;
+        content: "\00a0";
+        display: inline-block;
+        font: 16px/1em sans-serif;
+        height: 16px;
+        margin-right: 10px;
+        vertical-align: top;
+        width: 16px;
+    }
+    input[type="checkbox"]:checked + label:before {
+        background: #fff;
+        color: #0067a0;
+        content: "\2713";
+        text-align: center;
+    }
+    input[type="checkbox"]:checked + label:after {
+        font-weight: bold;
+    }
+    input[type="checkbox"]:focus + label::before {
+        outline: rgb(59, 153, 252) auto 5px;
     }
 </style>
 @section('scripts')
@@ -113,4 +164,3 @@
     <script src="https://rawgit.com/jasondavies/d3-cloud/master/build/d3.layout.cloud.js"></script>
     <script src="{{ asset('js/pages/word-clouds.js') }}?time={{ time() }}"></script>
 @endsection
-
