0d826a7167615846281a41017baf457bbf28a117
Anton-Matynin <Anton.matyunin1101@outlook.com>
2020-04-22 06:07:01 -0700

    fix the style issue

--- resources/views/pages/capture/capture-audio.blade.php
+++ resources/views/pages/capture/capture-audio.blade.php
@@ -6,18 +6,17 @@
     <div class="container">
         <div class="kt-portlet">
             <div class="kt-portlet__body">
-
                 <center><h1 class="display-4">Share your thoughts and feelings in audio</h1></center>
-
                 <div>
                     <div class="recorder_wrapper">
-                        <div class="recorder record_btn" id="recorder" style="margin: auto; text-align: center;justify-content: center; align-items: center;display: flex">
-
+                        <div class="recorder record_btn" id="recorder"
+                             style="margin: auto; text-align: center;justify-content: center; align-items: center;display: flex">
                         </div>
                     </div>
 
                     <!-- Modal -->
-                    <div class="modal fade" id="reviewRecordingModal" tabindex="-1" role="dialog" aria-labelledby="reviewRecordingModal" aria-hidden="true">
+                    <div class="modal fade" id="reviewRecordingModal" tabindex="-1" role="dialog"
+                         aria-labelledby="reviewRecordingModal" aria-hidden="true">
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
@@ -27,40 +26,44 @@
                                     </button>
                                 </div>
                                 <div class="modal-body">
-                                    <div id="recordingsList" />
+                                    <div id="recordingsList"></div>
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary btn-danger" data-dismiss="modal">Delete Recording</button>
+                                    <button type="button" class="btn btn-secondary btn-danger" data-dismiss="modal">
+                                        Delete Recording
+                                    </button>
                                     <button class="btn btn-success" id="uploadInfo">Save Recording</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
                 <br>
                 <br>
-
-                <p><b>Please share your fears, frustrations, needs, and hopes,
-                and what is helping you get through this crisis.</b>
-                Your recording can be up to 5 minutes long.
-                You will be able to save the recording for private use
-                or share it publicly, but
-                please avoid using any identifying names or information.
-                It will be transcribed by Google's transcription service,
-                and you will be able to view a word cloud created from the transcript.
-                We hope that you will come back and record more as a journal of your thoughts and experiences.</p>
+                <p>
+                    <b>Please share your fears, frustrations, needs, and hopes,
+                        and what is helping you get through this crisis.</b>
+                    Your recording can be up to 5 minutes long.
+                    You will be able to save the recording for private use
+                    or share it publicly, but
+                    please avoid using any identifying names or information.
+                    It will be transcribed by Google's transcription service,
+                    and you will be able to view a word cloud created from the transcript.
+                    We hope that you will come back and record more as a journal of your thoughts and experiences.</p>
 
                 <p>If you would prefer, you can
-                <a href="{{ route('capture-video') }}">record video</a> or
-                <a href="{{ route('capture-text') }}">type text</a> instead.</p>
+                    <a href="{{ route('capture-video') }}">record video</a> or
+                    <a href="{{ route('capture-text') }}">type text</a> instead.</p>
 
                 <br>
-                <p><i><span class="form-text text-muted">Technical details: 
-		Audio recording works on Chrome, Firefox, and Safari browsers, 
-		but only on Safari in iOS.
-		The resulting audio files are single channel with 44.1 kHz 
-		sample rate and 16 bits per sample.</span></i></p>
+                <p>
+                    <i><span class="form-text text-muted">Technical details:
+                    Audio recording works on Chrome, Firefox, and Safari browsers,
+                    but only on Safari in iOS.
+                    The resulting audio files are single channel with 44.1 kHz
+                    sample rate and 16 bits per sample.</span>
+                    </i>
+                </p>
 
             </div>
         </div>
