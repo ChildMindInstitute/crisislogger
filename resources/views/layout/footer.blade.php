11ebad53ac58447bf71a633dd78d05c8c5716267
Anton-Matynin <Anton.matyunin1101@outlook.com>
2020-04-22 00:30:44 -0700

    fix the privacy and other style

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,18 +1,18 @@
-<div class="kt-footer  kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
-    <div class="kt-container  kt-container--fluid ">
+<footer class="kt-footer  kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
+    <div class="container  kt-container--fluid ">
         <div class="kt-footer__copyright" style="width: 100%; padding-top:25px;">
-            <div class="row text-center" style="width: 100%;justify-content: center">
-                <p class="col-lg-1 col-md-4 col-sm-12"><a href="/privacy.html" style="color: #74788d !important;">Privacy</a></p>
-                <p class="col-lg-1 col-md-4 col-sm-12">©2020 Child Mind Institute</p>
-                <p class="col-lg-1 col-md-4 col-sm-12"><img class="third-party-logos" src="{{ asset('media/logos/CMI_spot_logo.jpg') }}" alt="" style="max-height:50px;" /></p>
-                <p class="col-lg-1 col-md-4 col-sm-12"><img class="third-party-logos" src="{{ asset('media/logos/parents_magazine_logo.png') }}" alt="" style="max-height:20px;" /></p>
-                <p class="col-lg-1 col-md-4 col-sm-12"><img class="third-party-logos" src="{{ asset('media/logos/nimh-logo.png') }}" alt="" style="max-height:30px;" /></p>
-                <p class="col-lg-1 col-md-4 col-sm-12"><img class="third-party-logos" src="{{ asset('media/logos/open-humans.png') }}" alt="" style="max-height:30px;" /></p>
-                <p class="col-lg-1 col-md-4 col-sm-12"><img class="third-party-logos" src="{{ asset('media/logos/CRI.png') }}" alt="" style="max-height:50px;" /></p>
+            <div class="row text-center" style="width: 100%;justify-content: space-around;">
+                <p class="col-lg-1 col-md-4 col-sm-12 mb-4"><a href="/privacy.html" style="color: #74788d !important;">Privacy</a></p>
+                <p class="col-lg-1 col-md-4 col-sm-12 mb-4">©2020 Child Mind Institute</p>
+                <p class="col-lg-1 col-md-4 col-sm-12 mb-4"><img class="third-party-logos" src="{{ asset('media/logos/CMI_spot_logo.jpg') }}" alt="" style="max-height:50px;" /></p>
+                <p class="col-lg-1 col-md-4 col-sm-12 mb-4"><img class="third-party-logos" src="{{ asset('media/logos/parents_magazine_logo.png') }}" alt="" style="max-height:20px;" /></p>
+                <p class="col-lg-1 col-md-4 col-sm-12 mb-4"><img class="third-party-logos" src="{{ asset('media/logos/nimh-logo.png') }}" alt="" style="max-height:30px;" /></p>
+                <p class="col-lg-1 col-md-4 col-sm-12 mb-4"><img class="third-party-logos" src="{{ asset('media/logos/open-humans.png') }}" alt="" style="max-height:30px;" /></p>
+                <p class="col-lg-1 col-md-4 col-sm-12 mb-4"><img class="third-party-logos" src="{{ asset('media/logos/CRI.png') }}" alt="" style="max-height:50px;" /></p>
             </div>
         </div>
     </div>
-</div>
+</footer>
 <script>
     var KTAppOptions = {
         "colors": {
