25e1790cc21e226da12f9678181876bc6e8bf7a5
Anton-Matynin <anton.matyunin1101@outlook.com>
2020-05-22 14:00:29 +0300

    fix the issue

--- app/Http/Controllers/TranscribeController.php
+++ app/Http/Controllers/TranscribeController.php
@@ -27,7 +27,7 @@ class TranscribeController extends Controller {
         if ($searchTxt != null) {
             $transcriptions = $transcriptions->where('transcriptions.text', 'like', "%$searchTxt%");
         }
-        $texts = Texts::select(\DB::raw('"null" as name, text, share, hide, false as converted, created_at, (id+ "-text") as id'))
+        $texts = Texts::select(\DB::raw('"null" as name, text, share, hide, false as converted, text.created_at, (id+ "-text") as id'))
             ->where('hide', '=', '0')
             ->where('share', '>', '0');
         if ($searchTxt != null) {
