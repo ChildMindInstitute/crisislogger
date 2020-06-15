ca135674d5af6180030e3866fd82ec39afa953b2
Anton-Matynin <anton.matyunin1101@outlook.com>
2020-06-15 05:03:54 +0300

    fix subdomain file change

---
+++ resources/views/pages/subdomain/racism-in-academia/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layout.app')
+@section('title', 'Home')
+@section('description', '')
+@section('content')
+    <div class="container kt-portlet pt-0">
+        <p>Subdomain</p>
+        <img src="{{ asset('media/photos/crisislogger-banner-plus-logo.png') }}" alt="">
+        <a href="{{ route('login') }}" class="login-btn text-white btn-lg">Login</a>
+        <div class="text-center">
+            <br>
+            <h1 class="display-4">Please tell us how you're feeling during this COVID-19 crisis</h1>
+        </div>
+
+
+        <div class="text-center" style="margin-bottom: 30px;margin-top: 30px">
+            <a href="{{ route('capture-video') }}" class="btn-primary btn btn-wide btn-lg mb-4">Share Your Thoughts</a>
+        </div>
+
+        <p>This is an incredibly challenging time,
+            and the pandemic has transformed our lives in ways we never
+            could have imagined.
+            We’re concerned about the health of our loved ones,
+            grateful for the medical heroes and essential workers
+            in our communities, and trying our best to adapt to this
+            new normal. We want to be optimistic,
+            but the ongoing stress and uncertainty is
+            affecting us all, and it’s hard not to worry about what
+            the future holds.</p>
+
+        <p>We are here to listen and learn from your experience.
+            We invite you to share your thoughts with us —
+            including your fears, frustrations, and hopes —
+            as an audio or video recording by clicking the "Share Your Thoughts"
+            link above.
+            Explore what others have recorded by clicking the "Listen to Others' Thoughts".
+            If you choose to contribute your recording to science,
+            it will help us find the best ways to provide support to families.</p>
+
+        <p>The Child Mind Institute and <i>Parents</i> Magazine
+            have partnered with
+            the National Institute of Mental Health, OpenHumans.org,
+            the CRI - Université de Paris,
+            and Satrajit Ghosh and Sanu Abraham at MIT
+            on this important research project.</p>
+
+        <p><span class="text-muted">CrisisLogger comes out
+	of the <a href="https://matter.childmind.org">MATTER Lab</a>
+	at the Child Mind Institute.</span></p>
+
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,25 @@ use App\Upload;
 Route::group(array('domain' => '{subdomain}.'.config('app.app_domain').''), function () {
     Route::get('/', function ($subdomain) {
         session()->put('subdomain', $subdomain);
-        return view('pages.index');
+        if (view()->exists('pages.subdomain.'.$subdomain.'.index'))
+        {
+            return view('pages.subdomain.'.$subdomain.'.index');
+        }
+        return App::abort(404);
     });
     Route::prefix('capture')->group(function () {
-        Route::get('/audio', function () {
-            return view('pages.subdomain.capture.capture-audio');
+        Route::get('/audio', function ($subdomain) {
+            return view('pages.subdomain.'.$subdomain.'.capture.capture-audio');
         })->name('capture-audio');
-        Route::get('video', function () {
-            return view('pages.subdomain.capture.capture-video');
+        Route::get('video', function ($subdomain) {
+            return view('pages.subdomain.'.$subdomain.'.capture.capture-video');
         })->name('capture-video');
-        Route::get('text', function () {
-            return view('pages.subdomain.capture.capture-text');
+        Route::get('text', function ($subdomain) {
+            return view('pages.subdomain.'.$subdomain.'.capture.capture-text');
         })->name('capture-text');
     });
-    Route::get('questionnaire', function () {
-        return view('pages.subdomain.questionnaire.index');
+    Route::get('questionnaire', function ($subdomain) {
+        return view('pages.subdomain.'.$subdomain.'.questionnaire.index');
     })->name('questionnaire');
 });
 Route::get('/', function () {
