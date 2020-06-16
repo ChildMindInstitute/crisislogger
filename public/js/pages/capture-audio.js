2fe92f01a097d92ec74f26bf129cb5fab837f620
Anton-Matynin <anton.matyunin1101@outlook.com>
2020-06-16 12:59:02 +0300

    fix issue

--- public/js/pages/capture-audio.js
+++ public/js/pages/capture-audio.js
@@ -1,159 +1,3 @@
-var video, timeoutRequest, reqBtn, recordButton, stream, recorder, uploadBtn;
-video = document.getElementById('video');
-video.setAttribute('style', 'width: 100%');
-reqBtn = document.getElementById('cameraButton');
-reqBtn.onclick = requestVideo;
-recordButton = document.getElementById('button');
-videoContainer = document.getElementById('recordingsList');
-uploadBtn = document.getElementById('uploadInfo');
-
-let videoUpload = document.getElementById('upload');
-let preview = document.getElementById('live-video');
-let spinner = document.getElementById('spinner');
-
-let isRecording = false;
-const camIcon = `
-<svg class="bi bi-camera-video-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2.667 3h6.666C10.253 3 11 3.746 11 4.667v6.666c0 .92-.746 1.667-1.667 1.667H2.667C1.747 13 1 12.254 1 11.333V4.667C1 3.747 1.746 3 2.667 3z"/>
-    <path d="M7.404 8.697l6.363 3.692c.54.313 1.233-.066 1.233-.697V4.308c0-.63-.693-1.01-1.233-.696L7.404 7.304a.802.802 0 000 1.393z"/>
-</svg>
-`;
-const videoStopIcon = `
-<svg id="stop-icon" class="bi bi-stop-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path d="M5 3.5h6A1.5 1.5 0 0112.5 5v6a1.5 1.5 0 01-1.5 1.5H5A1.5 1.5 0 013.5 11V5A1.5 1.5 0 015 3.5z"/>
-</svg>
-`;
-recordButton.addEventListener("click", toggleRecording);
-
-function toggleRecording() {
-    if (isRecording) {
-        startVideoRecording();
-        isRecording = false;
-    } else {
-        startRecording();
-    }
-}
-
-function requestVideo() {
-
-    if (document.getElementById('error-id'))
-    {
-        document.getElementById('error-id').remove();
-    }
-    spinner.classList.remove('d-none');
-
-    let constraints = {
-        audio: true,
-        video: {
-            width: {min: '100%', ideal: 320},
-            height: {min: 240, ideal: 240},
-        }
-    };
-
-    navigator.mediaDevices.getUserMedia(constraints).then(stm => {
-        stream = stm;
-        reqBtn.style.display = 'none';
-        button.removeAttribute('disabled');
-        preview.srcObject = stream;
-        preview.captureStream = preview.captureStream || preview.mozCaptureStream;
-        preview.classList.remove('d-none');
-        spinner.classList.add('d-none');
-        recordButton.style.display = "inline-block";
-    }).catch(e => {
-        if (e)
-        {
-
-            let errorMsg = 'We can not find the camera device. please check and try again';
-            let error = document.createElement('p');
-            error.innerText =  errorMsg;
-            error.setAttribute('id', 'error-id');
-            error.classList.add('error');
-            reqBtn.after(error);
-            spinner.classList.add('d-none');
-        }
-    });
-}
-
-function startVideoRecording() {
-    isRecording = true;
-    button.innerHTML = videoStopIcon;
-    button.classList.add('recording');
-    if (document.getElementById('error-id'))
-    {
-        document.getElementById('error-id').remove();
-    }
-    if ( navigator.vibrate ) navigator.vibrate( 150 );
-
-    preview.classList.remove('d-none');
-    try {
-        recorder = new MediaRecorder(stream, {
-            mimeType: 'video/webm'
-        });
-        recorder.start();
-        timeoutRequest = setTimeout(function() {
-            console.log('Recording time limit reached')
-            stopRecording();
-        }, 300000);
-    }
-    catch (e) {
-        isRecording = false
-        button.classList.remove('recording');
-        button.innerHTML = camIcon;
-        let errorMsg = 'Seems like your browser does\'t support video recording, please try to use Chrome or Firefox';
-        let error = document.createElement('p');
-        error.innerText =  errorMsg;
-        error.setAttribute('id', 'error-id');
-        error.classList.add('error');
-        recordButton.after(error);
-    }
-
-
-    //limit recording to 5 mins = 300,000 ms
-    timeoutRequest = setTimeout(function() {
-        stopVideoRecording();
-    }, 300000);
-}
-
-
-function stopVideoRecording() {
-    console.log('recording stopped');
-    isRecording = false;
-    button.classList.remove('recording');
-    button.innerHTML = camIcon;
-    $("#reviewRecordingModal").modal({
-        backdrop: 'static',
-        keyboard: false
-    });
-    if ( navigator.vibrate ) navigator.vibrate( [ 200, 100, 200 ] );
-    const chunks = [];
-    preview.classList.add('d-none');
-    recorder.ondataavailable = e => {
-        chunks.push(e.data);
-        video.src = URL.createObjectURL(e.data);
-        var blob = new Blob(chunks, {type: 'video/webm'});
-        let filename = new Date().toISOString() + ".webm";
-
-        videoUpload.addEventListener('click', async (e) => {
-            e.preventDefault();
-            if (!$('input[name="share"]').is(':checked'))
-            {
-                $('.share-section').after().find('.invalid-feedback').remove();
-                $('.share-section').after().append("<span class='invalid-feedback'>You need to choose one of the above options.</span>").show()
-                return false;
-            }
-            if (!$('#years-old').is(':checked'))
-            {
-
-                $('#years-old-label').after().find('.invalid-feedback').remove();
-                $('#years-old-label').after("<span class='invalid-feedback'>You need to click above checkbox before continue.</span>").show()
-                return false;
-            }
-            uploadButtonClicked(blob, filename);
-        });
-    };
-    recorder.stop();
-    clearTimeout(timeoutRequest);
-}
 //webkitURL is deprecated but nevertheless
 URL = window.URL || window.webkitURL;
 let gumStream;
@@ -166,6 +10,8 @@ let input;
 let AudioContext = window.AudioContext || window.webkitAudioContext;
 let audioContext = new AudioContext;
 //new audio context to help us record
+let timeoutRequest;
+let isRecording = false;
 let button = document.getElementById("recorder");
 const micIcon = "<i class=\"fa fa-microphone\" style='text-align: center;margin: 0 auto'></i>\n"
 const stopIcon = `
@@ -178,9 +24,9 @@ let recordingsList = document.getElementById("recordingsList");
 let upload = document.getElementById('upload');
 let upload_info = document.getElementById('uploadInfo');
 //add events to those 3 buttons
-button.addEventListener("click", audioToggleRecording);
+button.addEventListener("click", toggleRecording);
 
-function audioToggleRecording() {
+function toggleRecording() {
     if (isRecording) {
         stopRecording();
         isRecording = false;
