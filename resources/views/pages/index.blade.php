5fd8d1dce9d5c1ffa8acfc19d1d3e082ad7b681a
Anton-Matynin <anton.matyunin1101@outlook.com>
2020-06-17 13:10:53 +0300

    fix issue

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -15,6 +15,7 @@
 
         <div class="text-center" style="margin-bottom: 30px;margin-top: 30px">
             <a href="{{ route('capture-choice') }}" class="btn-primary btn btn-wide btn-lg mb-4">Share Your Thoughts</a>
+            <a href="https://explore.crisislogger.org" class="btn-outline-primary btn btn-wide btn-lg mb-4">Listen to Others' Thoughts</a>
         </div>
 
 	<p>This is an incredibly challenging time,
